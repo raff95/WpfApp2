0a26bc628dc073228dfcdc4d2ee3539ecea02606
Student <Student@it1587>
2020-06-15 12:12:31 +0200

    close_button

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
